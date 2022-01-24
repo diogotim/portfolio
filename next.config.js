86749cb208a86f37043716dfadef8eeccd2fbe52
diogotim <98195230+diogotim@users.noreply.github.com>
2022-01-24 12:33:15 -0300

    add base path

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isProd ? '/portfolio' : '',
   assetPrefix: isProd ? 'https://diogotim.github.io/portfolio/' : '',
 };
 
