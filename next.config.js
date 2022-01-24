5f0246549795ddffd161608149d2f0890f8f66d9
diogotim <98195230+diogotim@users.noreply.github.com>
2022-01-24 12:26:58 -0300

    add asset prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://diogotim.github.io/portfolio/' : '',
 };
 
 module.exports = nextConfig;
